55fa62a36fad0c50d0f17c967b534260a31ddf6e
PamelaKelly <pamelakelly7@gmail.com>
2017-03-17 23:09:24 +0000

    initial scraper script

--- src/dublin-bikes/scaper.py
+++ src/dublin-bikes/scaper.py
@@ -1,1 +1,14 @@
+import requests
+import json
+from pprint import pprint
+
+file = "c:\\Users\\pamel\\Desktop\\db-apikey.txt"
+fh = open(file)
+APIKEY = fh.readline().strip()
+
+NAME = "Dublin"
+URI = "https://api.jcdecaux.com/vls/v1/stations"
+r = requests.get(URI, params={"apiKey": APIKEY, "contract": NAME})
+
+pprint(json.loads(r.text))
 
