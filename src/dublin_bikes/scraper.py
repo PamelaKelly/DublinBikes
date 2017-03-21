198aa2c63a014afdff290d78c39057d30c8fe76b
PamelaKelly <pamelakelly7@gmail.com>
2017-03-21 12:15:03 +0000

    changed pkg name to remove dash, added pkgs to PYTHONPATH, started tests

---
+++ src/dublin_bikes/scraper.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from pprint import pprint
+import time
+import datetime
+
+#helper function to deal with datetime
+def datetime_formatter(timestamp):
+    dt = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    return dt
+
+file = "c:\\Users\\pamel\\Desktop\\db-apikey.txt"
+fh = open(file)
+APIKEY = fh.readline().strip()
+NAME = "Dublin"
+URI = "https://api.jcdecaux.com/vls/v1/stations"
+i = 0
+print(time.time())
+while True:
+    r = requests.get(URI, params={"apiKey": APIKEY, "contract": NAME})
+    with open('db-data' + str(i) + '.txt', 'w') as outfile:
+        json.dump(r.text, outfile)
+    i += 1
+    time.sleep(300)
+
+pprint(json.loads(r.text))
---
+++ src/tests/tests.py
@@ -0,0 +1,34 @@
+"""Scraper Tests"""
+from dublin_bikes import scraper
+import time
+
+def test_datetime_formatter(timestamp, correct_date):
+    try:
+        str_dt = datetime_formatter(timestamp)
+        dt_split = str_dt.split()
+        if dt_split[0] == correct_date:
+            print("You have converted to the correct date")
+        time = dt_split[1].split(':')
+        if time[0] == correct_date[0] and time[1] == correct_date[1]:
+            print("You have converted the time correctly to the nearest minute")
+    except:
+        print("You have an error in your datetime formatter function")
+        
+#sample data and test calls
+#testing datetime_formatter() function
+timestamp = 1490097572.2960699
+test_datetime_formatter(timestamp, ['11', '59'])
+
+    
+######################################################
+"""Database Tests"""
+
+######################################################
+"""Flask Tests"""
+
+######################################################
+"""Do we need any front end tests here as Aonghus
+did say it is harder to test and may have to be done
+through inspect element tool in browswer?"""
+
+######################################################
