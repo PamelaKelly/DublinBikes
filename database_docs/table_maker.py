3894af1dc1096c911b01eee2bbd0485b81997877
Katherine Campbell <katherine.campbell@ucdconnect.ie>
2017-03-30 15:45:03 +0100

    bonus fixed

--- database_docs/table_maker.py
+++ database_docs/table_maker.py
@@ -13,7 +13,7 @@ def make_db_tables():
         with conn.cursor() as cursor:
             #use database and create new record
             #create static data table
-            sql = "CREATE TABLE IF NOT EXISTS bike_stations(station_number INT(11) NOT NULL,station_name VARCHAR(45) NOT NULL,station_address VARCHAR(45) NOT NULL,station_location FLOAT NOT NULL,banking_available TINYINT(1) NOTNULL, PRIMARY KEY (station_number),FOREIGN KEY (station_number) REFERENCES availability(station_number)"
+            sql = "CREATE TABLE IF NOT EXISTS bike_stations(station_number INT(11) NOT NULL,station_name VARCHAR(45) NOT NULL,station_address VARCHAR(45) NOT NULL,station_location FLOAT NOT NULL,banking_available TINYINT(1) NOT NULL,bonus TINYINT(1), PRIMARY KEY (station_number),FOREIGN KEY (station_number) REFERENCES availability(station_number)"
             cursor.execute(sql)
     
         #must commit to save changes
