e11e03e5b9af9ed7625540ac54811733f1500fa4
Katherine Campbell <katherine.campbell@ucdconnect.ie>
2017-03-30 20:49:31 +0100

    I think it is fixed

--- database_docs/table_maker.py
+++ database_docs/table_maker.py
@@ -13,7 +13,7 @@ def make_db_tables():
         with conn.cursor() as cursor:
             #use database and create new record
             #create static data table
-            sql = "CREATE TABLE IF NOT EXISTS bike_stations(station_number INT(11) NOT NULL,station_name VARCHAR(45) NOT NULL,station_address VARCHAR(45) NOT NULL,station_location FLOAT NOT NULL,banking_available TINYINT(1) NOT NULL,bonus TINYINT(1), PRIMARY KEY (station_number),FOREIGN KEY (station_number) REFERENCES availability(station_number)"
+            sql = "CREATE TABLE bike_stations(station_number INT NOT NULL,station_name VARCHAR(45) NOT NULL,station_address VARCHAR(45) NOT NULL,station_location FLOAT NOT NULL,banking_available TINYINT(1) NOT NULL,bonus TINYINT(1), PRIMARY KEY (station_number)"
             cursor.execute(sql)
     
         #must commit to save changes
@@ -21,7 +21,7 @@ def make_db_tables():
 
         #create dynamic data table
         with conn.cursor() as cursor:
-            sql = "CREATE TABLE IF NOT EXISTS availability(station_number INT(11) NOT NULL,bike_stands INT(11) NOT NULL,bike_stands_available INT(11) NOT NULL,bikes_available INT(11) NOT NULL,last_updated TIMESTAMP () NOTNULL, PRIMARY KEY (station_number),FOREIGN KEY (station_number) REFERENCES bike_stations(station_number)"
+            sql = "CREATE TABLE availability(station_number INT NOT NULL, bike_stands INT NOT NULL,bike_stands_available INT NOT NULL,bikes_available INT NOT NULL,last_updated TIMESTAMP () NOTNULL, PRIMARY KEY (station_number),FOREIGN KEY (station_number) REFERENCES bike_stations(station_number)"
             cursor.execute(sql)
         conn.commit()
     finally:
