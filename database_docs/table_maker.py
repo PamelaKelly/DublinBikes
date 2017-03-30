2acb446ec62c0d7ab161c3faf610f0a0e13224ba
PamelaKelly <pamelakelly7@gmail.com>
2017-03-30 22:02:57 +0100

    fixed connection bugs and sql bug

--- database_docs/table_maker.py
+++ database_docs/table_maker.py
@@ -1,19 +1,25 @@
 import pymysql 
 
 def make_db_tables():
-    try:
-        #connect to db
-        file = "dbPassword.txt"
-        fh = open(file)
-        PASSWORD = fh.readline().strip()
-        conn=pymysql.connect(host="dublinbikeprojectdb.cun91scffwzf.eu-west-1.rds.amazonaws.com:3306",
+    #connect to db
+    file = "dbPassword.txt"
+    fh = open(file)
+    PASSWORD = fh.readline().strip()
+    conn=pymysql.connect(host="dublinbikeprojectdb.cun91scffwzf.eu-west-1.rds.amazonaws.com",
                              user="theForkAwakens",password= PASSWORD, db= 'DublinBikeProjectDB',
-                             charset= "utf8mb4", 
+                             port = 3306, charset= "utf8mb4", 
                              cursorclass=pymysql.cursors.DictCursor)
+    try:
         with conn.cursor() as cursor:
             #use database and create new record
             #create static data table
-            sql = "CREATE TABLE bike_stations(station_number INT NOT NULL,station_name VARCHAR(45) NOT NULL,station_address VARCHAR(45) NOT NULL,station_location FLOAT NOT NULL,banking_available TINYINT(1) NOT NULL,bonus TINYINT(1), PRIMARY KEY (station_number)"
+            sql = """CREATE TABLE bike_stations
+            (station_number INT NOT NULL,station_name VARCHAR(45) NOT NULL,
+            station_address VARCHAR(45) NOT NULL, 
+            station_location FLOAT NOT NULL, 
+            banking_available TINYINT(1) NOT NULL, 
+            bonus TINYINT(1), 
+            PRIMARY KEY (station_number));"""
             cursor.execute(sql)
     
         #must commit to save changes
@@ -26,6 +32,3 @@ def make_db_tables():
         conn.commit()
     finally:
         conn.close()
-
-
-make_db_tables()
