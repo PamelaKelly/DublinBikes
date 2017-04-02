bbf989c609207abaabdb7ba0fb4d56e8c1b280b4
PamelaKelly <pamelakelly7@gmail.com>
2017-04-02 18:49:53 +0100

    time zone issue resolved, Object Mapping Completed

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -3,38 +3,81 @@ import json
 import time
 import datetime
 import sqlalchemy
-from sqlalchemy import create_engine
-import calendar
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.mssql.base import TINYINT
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.dialects.mysql.types import FLOAT, VARCHAR
 #from IPython.display import display - not working
 
 #Need to make connection to db once and store connection in global object? - name of library? 
 
-class station:
-    def __init__(self, data):
-        self.station_number = data[0]
-        self.station_name = data[1]
-        self.station_address = data[2]
+Base = declarative_base()
+
+class Station(Base):
+    __tablename__ = 'bike_stations'
+    
+    station_number = Column(Integer, primary_key = True, nullable = False)
+    station_name = Column(String, nullable = False)
+    station_address = Column(String, nullable = False)
+    station_loc_lat = Column(FLOAT, nullable = False)
+    station_loc_long = Column(FLOAT, nullable = False)
+    banking_available = Column(TINYINT, nullable = False)
+    bonus = Column(TINYINT, nullable = False)
+    
+    def __repr__(self):
+        return """
+        <Station=(station_number=%s, 
+        station_name='%s',
+        station_address='%s',
+        station_loc_lat=%s,
+        station_loc_long=%s,
+        banking_available=%s,
+        bonus=%s)>""" % (self.station_number, self.station_name, 
+                         self.station_address, self.station_loc_lat,
+                         self.station_loc_long, self.banking_available,
+                         self.bonus)
+        
+class Station_Dynamic(Base):
+    __tablename__ = 'availability'
+    
+    station_number = Column(Integer, primary_key = True, nullable = False)
+    bike_stands = Column(Integer, nullable = False)
+    bike_stands_available = Column(Integer, nullable = False)
+    bikes_available = Column(Integer, nullable = False)
+    last_updated = Column(Integer, primary_key = True, nullable = False)
+    day = Column(VARCHAR, nullable = False)
+    
+    def __repr__(self):
+        return """
+        <Station_Dynamic=(station_number=%s,
+        bike_stands=%s, 
+        bike_stands_available=%s,
+        bikes_available=%s,
+        last_updated=%s, 
+        day = %s)>""" % (self.station_number, self.bike_stands,
+                                self.bike_stands_available, self.bikes_available,
+                                self.last_updated, self.day)
+        
+    
     
 
-#helper function to deal with datetime
+
 def datetime_formatter(timestamp):
+    """Takes the current time and returns the date, time and day of the week"""
     dt = datetime.datetime.fromtimestamp(timestamp).strftime('%Y_%m_%d_%H_%M_%S')
-    return dt
+    day = datetime.datetime.fromtimestamp(timestamp).strftime('%a')
+    return dt, day
 
-def write_to_file(r):
-    """Writes to backup file in json format"""
-    timestamp = time.time()
-    dt = datetime_formatter(timestamp)
+        
+def write_to_file(data):
+    """Stores json data in a file with date and time as part of filename"""
+    dt = datetime_formatter(time.time())
     with open('db_data_' + dt + '.txt', 'w') as outfile:
-        json.dump(json.JSONDecoder().decode(r.text), outfile)
-
-def parse_json_data(response):
-    data = json.JSONDecoder().decode(response.text)
-    static_data = ()
-    dynamic_data = ()
-    return static_data, dynamic_data
+        json.dump(data, outfile)
     
 def connect_db():
+    """Connects to the database"""
     try:
         URI = "DublinBikeProjectDB.cun91scffwzf.eu-west-1.rds.amazonaws.com"
         PORT = "3306"
@@ -49,60 +92,44 @@ def connect_db():
         print("Error Type: ", type(e))
         print("Error Details: ", e)    
 
-def write_to_db(table, values):
-    """Writes rows to the database"""
-    #needs to be refactored to take table name, columns and values as arguments
+def write_to_db(data):
+    """Creates SQLAlchemy objects from json data and pushes these objects to the db as rows"""
+    day = datetime_formatter(time.time())[1]
+    engine = connect_db()
+    Session = sessionmaker(bind=engine)
+    session = Session()
     try:
-        #connect to db
-        engine = connect_db()
-        
-        #write data to bike_stations table
-        sql_1 = """INSERT INTO `bike_stations` 
-        (`station_number`, `station_name`, `station_address`, 
-        `station_location`, `banking_available`, `bonus`)
-        VALUES (%s, %s, %s, %s, %s, %s);
-        """
-        
-        #write data to availability table
-        sql_2 = """INSERT INTO `availability`
-        (`station_number`, `bike_stands`, `bike_stands_available`,
-        `bikes_available`, `last_updated`)
-        VALUES (%s, %s, %s, %s, %s);"""
-        
-        if table == 'bike_stations':
-            sql = sql_1
-        elif table == 'availability':
-            sql = sql_2
-        else:
-            print("Error: Invalid station name given")
-            return 
-        
-        engine.execute(sql, values)
+        for i in data:
+            banking = 1 if (data[0]['banking']) else 0
+            bonus = 1 if (data[0]['bonus']) else 0
+                
+            #don't actually need to instantiate object here - could use session.add_all directly but choosing
+            #to do it this way for readability
+            
+            station = Station(station_number = i['number'],
+                          station_name = i['name'], 
+                          station_address = i['address'], 
+                          station_loc_lat = i['position']['lat'],
+                          station_loc_long = i['position']['lng'],
+                          banking_available = banking,
+                          bonus = bonus)
+            
+            station_dynamic = Station_Dynamic(station_number = i['number'],
+                                              bike_stands = i['bike_stands'], 
+                                              bike_stands_available = i['available_bike_stands'],
+                                              bikes_available = i['available_bikes'],
+                                              last_updated = i['last_update'], 
+                                              day = day)
+            
+            session.add_all(station, station_dynamic)
+            session.commit()   
         
     except Exception as e:
         print("Error Type: ", type(e))
         print("Error Details: ", e)  
 
 def get_data():
-    file = "db-apikey.txt"
-    #file = "/home/ubuntu/anaconda3/envs/TheForkAwakens/db-apikey.txt"
-    fh = open(file)
-    APIKEY = fh.readline().strip()
-    NAME = "Dublin"
-    URI = "https://api.jcdecaux.com/vls/v1/stations"
-    r = requests.get(URI, params={"apiKey": APIKEY, "contract": NAME})
-    return r
-
-def run_scraper():
-    """Scrapes the dublin bikes api for json data"""
-    while True:
-        r = get_data()
-        #parse_json_data(r)
-        #write_to_db(table, values)
-        write_to_file(r)
-        time.sleep(300)
-
-def parse_test(): 
+    """Sends the request to the Dublin bikes API and returns a json file"""
     file = "db-apikey.txt"
     fh = open(file)
     APIKEY = fh.readline().strip()
@@ -110,6 +137,13 @@ def parse_test():
     URI = "https://api.jcdecaux.com/vls/v1/stations"
     r = requests.get(URI, params={"apiKey": APIKEY, "contract": NAME})
     data = json.JSONDecoder().decode(r.text)
-    
-    
-parse_test()
+    return data
+
+def run_scraper():
+    """Runs the overall program to scrape the data and store the data"""
+    while True:
+        data = get_data()
+        write_to_file(data)
+        write_to_db(data)
+        time.sleep(300)
+
