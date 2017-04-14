1fb435a02c223e5e97a6e07252a0da26ab7ca94f
PamelaKelly <pamelakelly7@gmail.com>
2017-04-14 11:05:57 +0100

    refactored scrapers so that code is modularised and non repetitive

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -8,11 +8,12 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.mssql.base import TINYINT
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.dialects.mysql.types import FLOAT, VARCHAR, TIMESTAMP
-import sys
-# from IPython.display import display - not working
+from sqlalchemy.dialects.mysql.types import FLOAT, VARCHAR
 
 # Need to make connection to db once and store connection in global object? - name of library? 
+# Design Flaw: If I run the scraper to test it - and then run it again a few mins later - while there won't
+# be duplicate entries in the database there will be duplicate files with different names because I'm 
+# passing time.time() to create the filename. 
 
 Base = declarative_base()
 
@@ -62,166 +63,14 @@ class Station_Dynamic(Base):
                         self.bike_stands_available, self.bikes_available,
                         self.last_updated, self.day)
 
-
-
-
-
 def datetime_formatter(info):
-    """Takes the current time and returns the date, time and day of the week"""
+    """Takes the current time and returns the date, time and day of the week as a string"""
     if type(info) == float:
         dt = datetime.datetime.fromtimestamp(info).strftime('%Y-%m-%d %H-%M-%S')
         day = datetime.datetime.fromtimestamp(info).strftime('%a')
-        return dt, day
+        return day, dt
     elif type(info) == str:
         return day_from_filename(info)
-
-
-def write_to_file(data):
-    """Stores json data in a file with date and time as part of filename"""
-    dt = datetime_formatter(time.time())[0]
-    with open('db-data-' + dt + '.txt', 'w') as outfile:
-        json.dump(data, outfile)
-
-def connect_db():
-    """Connects to the database"""
-    try:
-        URI = "DublinBikeProjectDB.cun91scffwzf.eu-west-1.rds.amazonaws.com"
-        PORT = "3306"
-        DB = "DublinBikeProjectDB"
-        USER = "theForkAwakens"
-        file = "db_password.txt"
-        #file = "../Anaconda3/envs/softwareEng/workspace/Assignment4-P-E-K/src/scraper/db_password.txt"
-        fh = open(file)
-        PASSWORD = fh.readline().strip()
-        engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB), echo=True)
-        print("Engine Type Being Returned ", type(engine))
-        return engine
-    except Exception as e:
-        print("Error Type: ", type(e))
-        print("Error Details: ", e)   
-
-def write_to_stations(data):
-    engine = connect_db()
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    try:
-        for i in data:
-            print("i is equal to ", i)
-            banking = 1 if (i['banking']) else 0
-            bonus = 1 if (i['bonus']) else 0
-
-            station = Station(station_number=i["number"],
-                                    station_name=i["name"],
-                                    station_address=i["address"],
-                                    station_loc_lat=i["position"]["lat"],
-                                    station_loc_long=i["position"]["lng"],
-                                    banking_available=banking,
-                                    bonus=bonus)
-            
-            print("station...", station)
-            session.add(station)
-            session.commit()
-        session.close()
-
-    except Exception as e:
-        print("Error Type: ", type(e))
-        print("Error Details: ", e)
-
-def write_to_availability_basic(data, filename):
-    #engine = connect_db()
-    day = datetime_formatter(filename)
-    
-    for i in data:
-        engine = connect_db()
-        station_number = i["number"]
-        bike_stands = i["bike_stands"]
-        bike_stands_available = i["available_bike_stands"]
-        bikes_available = i["available_bikes"]
-        last_updated = i["last_update"]
-        day = day
-
-        sql_insert = "INSERT INTO availability (station_number, bike_stands, bike_stands_available, bikes_available, last_updated, day) VALUES (%s, %s, %s, %s, %s, %s)"
-        try:
-            engine.execute(sql_insert, station_number, bike_stands, bike_stands_available, bikes_available, last_updated, day)
-            engine.close()
-        except Exception as e:
-            engine.close()
-            print("in the write_to_av_basic function...")
-            print("Error Type: ", type(e))
-            print("Error Details: ", e)
-            continue
-
-
-def write_to_availability(data, filename):
-    day = datetime_formatter(filename)
-    for i in data:
-        try:
-            engine = connect_db()
-            Session = sessionmaker(bind=engine)
-            session = Session()
-            station_dynamic = Station_Dynamic(station_number=int(i["number"]),
-                                bike_stands=int(i["bike_stands"]),
-                                bike_stands_available=int(i["available_bike_stands"]),
-                                bikes_available=int(i["available_bikes"]),
-                                last_updated=int(i["last_update"]),
-                                day=day)
-
-            print("station_dynamic...", station_dynamic)
-
-            session.add(station_dynamic)
-            print("after the add")
-            session.commit()
-            print("after the commit")
-            session.close()
-            
-        except Exception as e:
-            print("Error Type: ", type(e))
-            print("Error details: ", e)
-            session.close()
-            continue
-
-
-
-def file_to_db(file):
-    print(file)
-    """ Helper function to write data from file to database"""
-    try:
-        with open(file, 'r') as obj:
-            dataStr = json.load(obj)
-            dataJson = json.loads(dataStr)
-        write_to_availability(dataJson, file)
-    except Exception as e:
-        print("Error Type: ", type(e))
-        print("Error Details: ", e)
-      
-def multiple_files_to_db():
-    try:       
-        for file in os.listdir(os.getcwd()):
-            if file.endswith(".txt"):
-                file_to_db(file)
-    except Exception as e:
-        print("Error Type: ", type(e))
-        print("Error Details: ", e)
-
-def get_data():
-    """Sends the request to the Dublin bikes API and returns a json file"""
-    file = "./Assignment4-P-E-K/src/scraper/db-apikey.txt"
-    fh = open(file)
-    APIKEY = fh.readline().strip()
-    NAME = "Dublin"
-    URI = "https://api.jcdecaux.com/vls/v1/stations"
-    r = requests.get(URI, params={"apiKey": APIKEY, "contract": NAME})
-    data = json.JSONDecoder().decode(r.text)
-    return data
-
-def run_scraper():
-    """Runs the overall program to scrape the data and store the data"""
-    while True:
-        data = get_data()
-        write_to_file(data)
-        #write_to_db(data, time.time())
-        time.sleep(300)
 
 def day_from_filename(filename):
     word = filename.split()
@@ -235,5 +84,127 @@ def day_from_filename(filename):
     day = datetime.datetime.strptime(new_date, '%B %d, %Y').strftime('%a')
     return day
 
-multiple_files_to_db()
+def write_to_file(data):
+    """Stores json data in a file with date and time as part of filename"""
+    dt = datetime_formatter(time.time())[1]
+    with open('db-data-' + dt + '.txt', 'w') as outfile:
+        json.dump(data, outfile)
+
+def connect_db(URI, PORT, DB, USER, password_file):
+    #rename this function to get_engine or something
+    """Connects to the database"""
+    try:
+        fh = open(password_file)
+        PASSWORD = fh.readline().strip()
+        engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB), echo=True)
+        return engine
+    except Exception as e:
+        print("Error Type: ", type(e))
+        print("Error Details: ", e)   
+
+def write_to_stations(data, filename):
+    #filename not used here but want to use a callback in 
+    #write to file... 
+    engine = connect_db("DublinBikeProjectDB.cun91scffwzf.eu-west-1.rds.amazonaws.com", "3306", "DublinBikeProjectDB", "theForkAwakens", "db_password.txt")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for i in data:
+        try:
+            banking = 1 if (i['banking']) else 0
+            bonus = 1 if (i['bonus']) else 0
+
+            station = Station(station_number=i["number"],
+                                    station_name=i["name"],
+                                    station_address=i["address"],
+                                    station_loc_lat=i["position"]["lat"],
+                                    station_loc_long=i["position"]["lng"],
+                                    banking_available=banking,
+                                    bonus=bonus)
+            
+            session.add(station)
+            session.commit()
+
+        except Exception as e:
+            print("Error Type: ", type(e))
+            print("Error Details: ", e)
+            session.rollback()
+            continue
+    
+    session.close()
+    engine.dispose()
+
+def write_to_availability(data, day_source):
+    date_time = datetime_formatter(day_source)
+    day = date_time[0]
+    engine = connect_db("DublinBikeProjectDB.cun91scffwzf.eu-west-1.rds.amazonaws.com", "3306", "DublinBikeProjectDB", "theForkAwakens", "db_password.txt")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for i in data:
+        try:
+            station_dynamic = Station_Dynamic(station_number=int(i["number"]),
+                                bike_stands=int(i["bike_stands"]),
+                                bike_stands_available=int(i["available_bike_stands"]),
+                                bikes_available=int(i["available_bikes"]),
+                                last_updated=int(i["last_update"]),
+                                day=day)
+
+            session.add(station_dynamic)
+            session.commit()
+            
+        except Exception as e:
+            print("Error Type: ", type(e))
+            print("Error details: ", e)
+            session.rollback()
+            continue
+        
+    session.close()
+    engine.dispose()
+
+def file_to_db(file, write_function):
+    """ Helper function to write data from file to database
+    Given the file name and the function to pass the data to"""
+    try:
+        with open(file, 'r') as obj:
+            dataStr = json.load(obj)
+            dataJson = json.loads(dataStr)
+        write_function(dataJson, file)
+    except Exception as e:
+        print("Error Type: ", type(e))
+        print("Error Details: ", e)
+      
+def multiple_files_to_db():
+    try:       
+        for file in os.listdir(os.getcwd()):
+            if file.endswith(".txt"):
+                file_to_db(file)
+    except Exception as e:
+        print("Error Type: ", type(e))
+        print("Error Details: ", e)
+
+def get_data(key_file, URI, NAME):
+    """Sends the request to the Dublin bikes API and returns a json file"""
+    try:
+        fh = open(key_file)
+        APIKEY = fh.readline().strip()
+        if NAME == None:
+            r = requests.get(URI, params={"apiKey": APIKEY})
+        else:
+            r = requests.get(URI, params={"apiKey": APIKEY, "contract": NAME})
+        data = json.JSONDecoder().decode(r.text)
+        return data
+    except Exception as e:
+        print("Error Type: ", type(e))
+        print("Error Details: ", e)
+
+def run_scraper():
+    """Runs the overall program to scrape the data and store the data"""
+    while True:
+        data = get_data("db-apikey.txt", "https://api.jcdecaux.com/vls/v1/stations", "Dublin")
+        write_to_file(data)
+        write_to_availability(data, time.time())
+        time.sleep(300)
+
+
+run_scraper()
 
--- src/weather/weather_scraper.py
+++ src/weather/weather_scraper.py
@@ -1,28 +1,25 @@
-import requests
-import json
 import time
-import datetime
-import os
 import sqlalchemy
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.mssql.base import TINYINT
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.dialects.mysql.types import FLOAT, VARCHAR, TIMESTAMP
+from sqlalchemy.dialects.mysql.types import FLOAT, VARCHAR
+import scraper.scraper
 
 Base = declarative_base()
 
 class weather(Base):
     __tablename__ = 'weather_data'
     
-    date_time = Column(FLOAT, primary_key = True, nullable = False)
-    temp = Column(FLOAT, nullable = False)
-    temp_max = Column(FLOAT, nullable = False)
-    temp_min = Column(FLOAT, nullable = False)
-    humidity = Column(Integer, nullable = False)
-    main = Column(VARCHAR(45), nullable = False)
-    weather_description = Column(VARCHAR(45), nullable = False)
-    wind_speed= Column(Integer, nullable = False)
+    date_time = Column(FLOAT, primary_key=True, nullable=False)
+    temp = Column(FLOAT, nullable=False)
+    temp_max = Column(FLOAT, nullable=False)
+    temp_min = Column(FLOAT, nullable=False)
+    humidity = Column(Integer, nullable=False)
+    main = Column(VARCHAR(45), nullable=False)
+    weather_description = Column(VARCHAR(45), nullable=False)
+    wind_speed = Column(Integer, nullable=False)
     
     def __repr__(self):
         return """
@@ -34,7 +31,7 @@ class weather(Base):
         humidity=%d,
         main=%d,
         weather_description='%s',
-        wind_speed=%d)>""" % (self.date_time, self.temp, 
+        wind_speed=%d)>""" % (self.date_time, self.temp,
                          self.temp_max, self.temp_min,
                          self.humidity, self.main,
                          self.weather_description, self.wind_speed)
@@ -42,71 +39,54 @@ class weather(Base):
 def connect_weather_db():
     """Connects to the database"""
     try:
-        URI = "weatherdb.cnmhll8wqxlt.us-west-2.rds.amazonaws.com"
-        PORT = "3306"
-        DB = "WeatherDB"
-        USER = "Administrator"
-        file = "weatherDB_password.txt"
-        fh = open(file)
-        PASSWORD = fh.readline().strip()
-        engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB), echo = True)
+        engine = scraper.scraper.connect_db("weatherdb.cnmhll8wqxlt.us-west-2.rds.amazonaws.com", "3306", "WeatherDB", "Administrator", "weatherDB_password.txt")
         return engine
     except Exception as e:
         print("Error Type: ", type(e))
         print("Error Details: ", e)    
 
-
 def write_to_weather_db(data):
-	"""Creates SQLAlchemy objects from json data and pushes these objects to the db as rows"""
+    """Creates SQLAlchemy objects from json data and pushes these objects to the db as rows"""
 
-	engine = connect_weather_db()
-	Session = sessionmaker(bind=engine)
-	session = Session()
-	try:
-		weather_instance = weather(date_time = data["dt"],
-			temp = data["main"]["temp"], 
-			temp_max = data["main"]["temp_max"], 
-			temp_min = data["main"]["temp_min"],
-			humidity = data["main"]["humidity"],
-			main = data["weather"][0]["main"],
-			weather_description = data["weather"][0]["description"],
-			wind_speed = data["wind"]["speed"])
-
-		session.add(weather_instance)
-		session.commit()
-		session.close()   
-
-	except Exception as e:
-		print("in the write_to_weather_db function")
-		print("Error Type: ", type(e))
-		print("Error Details: ", e)  
-
-
-
-def get_weather_data():
-	"""Sends the request to the open weather API and returns a json file"""
-	try:
-		file = "weather_api_key.txt"
-		fh = open(file)
-		APIKEY = fh.readline().strip()
-		URI = "http://api.openweathermap.org/data/2.5/weather?q=dublin,ie&units=metric&appid="
-		r = requests.get(URI, params={"apiKey": APIKEY})
-		data = json.JSONDecoder().decode(r.text)
-		return data
-	except Exception as e:
-		print("in the get_weather_data")
-		print("Error type: ", type(e))
-		print("Error Details: ", e)
-	
-def run_weather_scraper():
+    engine = connect_weather_db()
+    Session = sessionmaker(bind=engine)
+    session = Session()
     try:
-        while True:
-            data = get_weather_data()
-            write_to_weather_db(data)
-            time.sleep(1800)
+        weather_instance = weather(date_time=data["dt"],
+			temp=data["main"]["temp"],
+			temp_max=data["main"]["temp_max"],
+			temp_min=data["main"]["temp_min"],
+			humidity=data["main"]["humidity"],
+			main=data["weather"][0]["main"],
+			weather_description=data["weather"][0]["description"],
+			wind_speed=data["wind"]["speed"])
+
+        session.add(weather_instance)
+        session.commit()
+        session.close()
+        engine.dispose()   
+
     except Exception as e:
         print("Error Type: ", type(e))
         print("Error Details: ", e)
+        session.close()
+        engine.dispose()  
+
+def get_weather_data():
+    """Sends the request to the open weather API and returns a json file"""
+    try:
+        data = scraper.scraper.get_data("weather_api_key.txt", "http://api.openweathermap.org/data/2.5/weather?q=dublin,ie&units=metric&appid=", NAME=None)
+        return data
+    except Exception as e:
+        print("Error type: ", type(e))
+        print("Error Details: ", e)
+
+def run_weather_scraper():
+    while True:
+        data = get_weather_data()
+        write_to_weather_db(data)
+        scraper.scraper.write_to_file(data)
+        time.sleep(1800)
 
 run_weather_scraper()
 
